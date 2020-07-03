4086ea5da3a44189030da6086282896e2177074c
devnoah0316 <noa316@naver.com>
2020-07-03 17:39:48 +0900

    add comments to help understanding

--- bg.js
+++ bg.js
@@ -4,9 +4,13 @@ const IMG_NUMBER = 6;
 
 
 function paintImage(imgNumber){
+    // image객체 생성
     const image = new Image();
+    // 속성 값 부여(파일 경로)
     image.src = `/images/${imgNumber + 1}.jpg`;
+    // classList에 추가하여 css이용 제어
     image.classList.add("bgImage");
+    // body의 제일 앞부분에 image 추가
     body.prepend(image);
 }
 
@@ -16,11 +20,9 @@ function genRandom(){
 }
 
 function init(){
-    // generate number
+    // generate random number
     const randomNumber = genRandom();
     paintImage(randomNumber);
 }
-
-
 
 init();
